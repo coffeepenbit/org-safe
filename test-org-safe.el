2ee50abc32d43d976826f8222ab8c198f8437c4b
coffeepenbit <coffeepenbit@gmail.com>
2021-03-25 16:26:09 -0400

    Refactored tests

--- test-org-safe.el
+++ test-org-safe.el
@@ -26,119 +26,147 @@
 (require 'ert)
 (ert-delete-all-tests)
 
+
 (if (featurep 'org-safe)
     (unload-feature 'org-safe t))
 (require 'org-safe "./org-safe.el")
 
+
+
+(defun ert-helper-org-buffer (buffer-text &optional func)
+  "Useful for testing org-mode functions in ert.
+
+BUFFER-TEXT is the initial state of the org-mode buffer.
+
+FUNC is what is ran after creating the buffer."
+  (with-temp-buffer
+    (insert buffer-text)
+    (beginning-of-buffer)
+    (org-mode)
+    (if func
+        (funcall func)
+      (buffer-string))
+    ))
+
+;;;; delete-backwards-char
 (ert-deftest test-delete-backward-char nil
   (should (string=
            "* headlin"
-           (with-temp-buffer
-             (insert "* headline")
-             (org-mode)
-             (goto-char (point-max))
-             (org-safe-delete-backward-char)
-             (buffer-string)))))
+           (ert-helper-org-buffer
+            "* headline"
+            (lambda nil
+              (goto-char (point-max))
+              (org-safe-delete-backward-char)
+              (buffer-string))
+            ))))
+
 
 (ert-deftest test-delete-backward-char-prevent-one-asterisk nil
   (should (string=
            "* headline"
-           (with-temp-buffer
-             (insert
-"* headline")
-             (org-mode)
-             (goto-char 2) ; After first asterisk
-             (org-safe-delete-backward-char)
-             (buffer-string)))))
+           (ert-helper-org-buffer
+            "* headline"
+            (lambda nil
+              (goto-char 2) ; After first asterisk
+              (org-safe-delete-backward-char)
+              (buffer-string))))))
+
 
 (ert-deftest test-delete-backward-char-prevent-two-asterisks nil
   (should (string=
            "** headline"
-           (with-temp-buffer
-             (insert
-"** headline")
-             (org-mode)
-             (goto-char 2) ; After first asterisk
-             (org-safe-delete-backward-char)
-             (buffer-string)))))
+           (ert-helper-org-buffer
+            "** headline"
+            (lambda nil
+              (org-mode)
+              (goto-char 2) ; After first asterisk
+              (org-safe-delete-backward-char)
+              (buffer-string))))))
+
 
 (ert-deftest test-delete-backward-char-allow-nonheadline-delete nil
   (should (string=
            "this is not a headline*"
-           (with-temp-buffer
-             (insert
-"*this is not a headline*")
-             (org-mode)
-             (goto-char 2) ; After first asterisk
-             (org-safe-delete-backward-char)
-             (buffer-string))))
+           (ert-helper-org-buffer
+            "*this is not a headline*"
+            (lambda nil
+              (goto-char 2) ; After first asterisk
+              (org-safe-delete-backward-char)
+              (buffer-string)))))
+
   (should (string=
            "*this is not a headline"
-           (with-temp-buffer
-             (insert
-"*this is not a headline*")
-             (org-mode)
-             (goto-char (point-max)) ; After last asterisk
-             (org-safe-delete-backward-char)
-             (buffer-string)))))
+           (ert-helper-org-buffer
+            "*this is not a headline*"
+            (lambda nil
+              (goto-char (point-max)) ; After last asterisk
+              (org-safe-delete-backward-char)
+              (buffer-string))))))
+
 
 (ert-deftest test-delete-backward-char-allow-nonheadline-delete-2 nil
   (should (string=
            "asterisk"
-           (with-temp-buffer
-             (insert
-"asterisk*")
-             (org-mode)
-             (goto-char (point-max)) ; After first asterisk
-             (org-safe-delete-backward-char)
-             (buffer-string)))))
+           (ert-helper-org-buffer
+            "asterisk*"
+            (lambda nil
+              (org-mode)
+              (goto-char (point-max)) ; After first asterisk
+              (org-safe-delete-backward-char)
+              (buffer-string))))))
 
 
+;;;; point-on-headline-stars
 (ert-deftest test-point-on-headline-stars nil
   (should (equal
            t
-           (with-temp-buffer
-             (insert
-"* headline")
-             (org-mode)
-             (goto-char 2) ; After first asterisk
-             (org-safe-point-on-headline-stars-p))))
+           (ert-helper-org-buffer
+            "* headline"
+            (lambda nil
+              (org-mode)
+              (goto-char 2) ; After first asterisk
+              (org-safe-point-on-headline-stars-p)))))
+
   (should (equal
            t
-           (with-temp-buffer
-             (insert
-"
-* headline")
-             (org-mode)
-             (goto-char (point-min)) ; At line above asterisk
-             (org-safe-point-on-headline-stars-p)))))
+           (ert-helper-org-buffer
+            "
+* headline"
+            (lambda nil
+              (org-mode)
+              (goto-char (point-min)) ; At line above asterisk
+              (org-safe-point-on-headline-stars-p))))))
 
 
 (ert-deftest test-point-not-on-headline-stars nil
   (should (equal
            nil
-           (with-temp-buffer
-             (insert
-"*headline")
-             (org-mode)
-             (goto-char 2) ; After first asterisk
-             (org-safe-point-on-headline-stars-p))))
+           (ert-helper-org-buffer
+            "*headline"
+            (lambda nil
+              (org-mode)
+              (goto-char 2) ; After first asterisk
+              (org-safe-point-on-headline-stars-p)))))
+
   (should (equal
            nil
-           (with-temp-buffer
-             (insert
-" * headline")
-             (org-mode)
-             (goto-char 3) ; After first asterisk
-             (org-safe-point-on-headline-stars-p))))
+           (ert-helper-org-buffer
+            " * headline"
+            (lambda nil
+              (org-mode)
+              (goto-char 3) ; After first asterisk
+              (org-safe-point-on-headline-stars-p)))))
+
   (should (equal
            nil
-           (with-temp-buffer
-             (insert
-"* headline*")
-             (org-mode)
-             (goto-char (point-max)) ; After first asterisk
-             (org-safe-point-on-headline-stars-p)))))
+           (ert-helper-org-buffer
+            "* headline*"
+            (lambda nil
+              (org-mode)
+              (goto-char (point-max)) ; After first asterisk
+              (org-safe-point-on-headline-stars-p))))))
+
+
 
 (ert t)
 
