dfafaa880f20fbf2da0fe5da8a927976399a57b0
coffeepenbit <coffeepenbit@gmail.com>
2021-04-05 09:41:14 -0400

    Added keyword to header

--- test-org-safe.el
+++ test-org-safe.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2021  coffeepenbit
 
 ;; Author: coffeepenbit@gmail.com
-;; Keywords:
+;; Keywords: outline
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
